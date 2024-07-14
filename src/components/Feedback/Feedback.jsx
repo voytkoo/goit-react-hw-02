cab3fd9a209fa18abc54ef43b06450747310d7b2
olena <avoytko86@gmail.com>
2024-07-14 16:10:58 +0300

    markup feedback

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -3,9 +3,9 @@ import s from "./Feedback.module.css";
 const Feedback = ({ feedback, total, positive }) => {
   return (
     <ul className={s.feedbackList}>
-      <li> Good: {s.good}</li>
-      <li> Neutral: {s.neutral}</li>
-      <li> Bad: {s.bad}</li>
+      <li> Good: {feedback.good}</li>
+      <li> Neutral: {feedback.neutral}</li>
+      <li> Bad: {feedback.bad}</li>
       <li> TotaL: {total}</li>
       <li>Positive: {positive}%</li>
     </ul>
